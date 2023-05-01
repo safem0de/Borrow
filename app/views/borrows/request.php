87f523b9797b398d33c2b0d21c97234ad873a1c1
safem0de <n.wangwanich@gmail.com>
2023-05-02 00:40:18 +0700

    format arrange

--- app/views/borrows/request.php
+++ app/views/borrows/request.php
@@ -1,11 +1,19 @@
 <!-- https://www.youtube.com/watch?v=GcpayqY7jag -->
+<!-- https://artisansweb.net/drag-drop-file-upload-using-javascript-php/ -->
 <!-- https://codepen.io/dcode-software/pen/xxwpLQo -->
+
 <?php require APPROOT . '/views/inc/header.php'; ?>
 
-<div class="drop-zone">
-    <span class="drop-zone__prompt">Drop file here or click to upload</span>
-    <input type="file" name="myFile" class="drop-zone__input">
-    <button type="button" class="btn btn-danger mt-2" id="clear-button" style="display:none;">Clear Image</button>
+<div class="row">
+    <div class="col-md-6 mx-auto my-3 text-center">
+        <h4>CSV file upload</h4>
+        <p>กรุณาลากวาง หรือ คลิ๊ก เพื่ออัพโหลดไฟล์</p>
+        
+        <div class="drop-zone">
+            <span class="drop-zone__prompt">Drag & drop file here or click to upload</span>
+            <input type="file" name="myFile" class="drop-zone__input">
+        </div>
+    </div>
 </div>
 
 <?php require APPROOT . '/views/inc/footer.php'; ?>
